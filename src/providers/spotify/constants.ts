67dc913814d2a83d140d3b371e2414f91e77a4ba
Jay Meistrich <github@jayz.us>
2025-12-26 20:45:26 +0900

    working on spotify

--- src/providers/spotify/constants.ts
+++ src/providers/spotify/constants.ts
@@ -1,3 +1,5 @@
+import Config from "react-native-config";
+
 export const SPOTIFY_AUTH_SCOPES = [
     "streaming",
     "user-read-email",
@@ -12,7 +14,9 @@ export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
 export const SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token";
 export const SPOTIFY_API_BASE = "https://api.spotify.com/v1";
 
-export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID ?? "";
-export const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI ?? "legendmusic://spotify-auth-callback";
+export const SPOTIFY_CLIENT_ID = Config.SPOTIFY_CLIENT_ID ?? "";
+export const SPOTIFY_REDIRECT_URI = "legendmusic://spotify-auth-callback";
 
 export const SPOTIFY_DEVICE_NAME = "Legend Music (Web Playback)";
+
+console.log("SPOTIFY_CLIENT_ID", SPOTIFY_CLIENT_ID);
